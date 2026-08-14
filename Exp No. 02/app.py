8168a35707708a1fd3869d96eb89cf1f268dbf5c
Aditya Gawade <adityagawade100262@gmail.com>
2026-08-14 19:02:14 +0530

    Add files via upload

---
+++ Exp No. 02/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+import os
+
+app = Flask(__name__)
+
+model = pickle.load(open("SLRModel.pkl", "rb"))
+
+@app.route("/")
+def home():
+    print("Current Working Directory:", os.getcwd())
+    print("Template exists:", os.path.exists("templates/index.html"))
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    cgpa = float(request.form["cgpa"])
+
+    prediction = model.predict(np.array([[cgpa]]))
+
+    return render_template(
+        "index.html",
+        prediction_text=f"Predicted Package : {prediction[0]:.2f} LPA"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
